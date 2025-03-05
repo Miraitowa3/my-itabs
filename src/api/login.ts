12991d7d36485caf656ed3a5145371ac5b8f238e
miraitowa <fengbb@sucsoft.com>
2025-03-05 17:59:47 +0800

    init

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,3 +2,4 @@ import { get,post} from './http';
 
 export  const getWxCode=()=>get('/wx/qrcode')
 export  const wxLogin=(data:any)=>post('/wx/login',data)
+export  const sendVerifyCode=(data:any)=>post('/user/sendVerifyCode',data)
--- src/views/login/components/Registry.vue
+++ src/views/login/components/Registry.vue
@@ -1,46 +1,44 @@
 <template>
-    <div class="registry">
-        <div class="mb-[40px] mt-4 text-center text-xl font-bold">注册</div>
-        <!-- <div class="bird-logo relative mt-[10px]">
-            <div class="brid-body"></div>
-            <span class="brid-claw left"></span>
-            <span class="brid-claw right"></span>
-        </div> -->
-        <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules">
-            <el-form-item prop="pass">
-                <div class="flex w-full items-center justify-around">
-                    <el-input v-model="ruleForm.emil" type="text" placeholder="请输入邮箱" @change="handleEmilChange" />
-                    <el-button type="primary" :disabled="!isEmil || isSendCode" class="ml-[5px] w-[90px]" @click="sendCode">{{ sendTimer }} </el-button>
-                </div>
-            </el-form-item>
-            <el-form-item prop="checkPass">
-                <el-input v-model="ruleForm.code" type="text" placeholder="请输入6位邮箱验证码" />
-            </el-form-item>
-            <el-form-item prop="age">
-                <el-input v-model="ruleForm.username" type="text" placeholder="请输入用户名" />
-            </el-form-item>
-            <el-form-item prop="age">
-                <el-input
-                    v-model="ruleForm.password"
-                    type="password"
-                    autocomplete="off"
-                    placeholder="请输入8 到 18 位密码"
-                    @blur="handlePasswordBlur"
-                    @focus="handlePasswordFocus"
-                />
-            </el-form-item>
-            <el-form-item>
-                <div class="h-10 w-full leading-none">
-                    <el-button type="primary" @click="submitForm(ruleFormRef)" style="height: 100%; width: 100%"> 立即注册 </el-button>
-                </div>
-            </el-form-item>
-        </el-form>
+  <div class="registry">
+    <!-- <div class="mb-[40px] mt-4 text-center text-xl font-bold">注册</div> -->
+    <div class="bird-logo relative mt-[10px]">
+      <div class="brid-body"></div>
+      <span class="brid-claw left"></span>
+      <span class="brid-claw right"></span>
     </div>
+    <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules">
+      <el-form-item prop="email">
+        <div class="flex w-full items-center justify-around">
+          <el-input v-model="ruleForm.email" type="text" placeholder="请输入邮箱" @change="handleEmilChange" clearable />
+          <el-button type="primary" :disabled="!isEmil || isSendCode" class="ml-[5px] w-[90px]"
+            @click="sendCode">{{ sendTimer }} </el-button>
+        </div>
+      </el-form-item>
+      <el-form-item prop="code">
+        <el-input v-model="ruleForm.code" type="text" placeholder="请输入6位邮箱验证码" clearable />
+      </el-form-item>
+      <el-form-item prop="username">
+        <el-input v-model="ruleForm.username" type="text" placeholder="请输入用户名" clearable />
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="ruleForm.password" type="password" autocomplete="off" placeholder="请输入8 到 18 位密码" clearable
+          @blur="handlePasswordBlur" @focus="handlePasswordFocus" />
+      </el-form-item>
+      <el-form-item>
+        <div class="h-10 w-full leading-none">
+          <el-button type="primary" @click="submitForm(ruleFormRef)" style="height: 100%; width: 100%"> 立即注册
+          </el-button>
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import type { FormInstance, FormRules } from "element-plus";
-import { validateEmil } from "@/utils/reg";
+import { validateEmil, validatePassword, validateCode, validateUsername } from "@/utils/reg";
+import { sendVerifyCode } from "@/api/login";
+
 const ruleFormRef = ref<FormInstance>();
 const isEmil = ref(false);
 const isSendCode = ref(false);
@@ -48,137 +46,187 @@ const CODE_TIMEOUT: number = 10;
 const sendTimer = ref<string | number>("获取验证码");
 const timer = ref<NodeJS.Timeout>();
 const ruleForm = ref({
-    emil: "",
-    code: "",
-    username: "",
-    password: "",
+  email: "",
+  code: "",
+  username: "",
+  password: "",
 });
 function handleEmilChange() {
-    //检测邮箱格式是否正确
+  //检测邮箱格式是否正确
 
-    if (validateEmil(ruleForm.value.emil)) {
-        isEmil.value = true;
-        sendTimer.value = "获取验证码";
-    } else {
-        isEmil.value = false;
-    }
+  if (validateEmil(ruleForm.value.email)) {
+    isEmil.value = true;
+    sendTimer.value = "获取验证码";
+  } else {
+    isEmil.value = false;
+  }
 }
-const rules = reactive<FormRules<typeof ruleForm>>({});
+const valiEmil = (rule: any, value: any, callback: any) => {
+  if (value === '' || !validateEmil(value)) {
+    callback('邮箱格式不正确')
+  } else {
+    callback()
+
+  }
+}
+const valiCode = (rule: any, value: any, callback: any) => {
+  if (value === '' || !validateCode(value)) {
+    callback('验证码格式不正确')
+  } else {
+    callback()
+
+  }
+}
+const vailPassword = (rule: any, value: any, callback: any) => {
+  if (value === '' || !validatePassword(value)) {
+    callback('密码格式不正确')
+  } else {
+    callback()
+
+  }
+}
+const vailUsername = (rule: any, value: any, callback: any) => {
+  if (value === '' || !validateUsername(value)) {
+    callback('用户名格式不正确')
+  } else {
+    callback()
+
+  }
+}
+const rules = reactive<FormRules<typeof ruleForm>>({
+
+  email: [{ validator: valiEmil, trigger: 'change' }],
+  code: [{ validator: valiCode, trigger: 'change' }],
+  username: [{ validator: vailUsername, trigger: 'change' }],
+  password: [{ validator: vailPassword, trigger: 'change' }],
+
+});
+
 
 const submitForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return;
-    formEl.validate((valid) => {
-        if (valid) {
-            console.log("submit!");
-        } else {
-            console.log("error submit!");
-        }
-    });
+  if (!formEl) return;
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log("submit!");
+    } else {
+      console.log("error submit!");
+    }
+  });
 };
 
 const resetForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return;
-    formEl.resetFields();
+  if (!formEl) return;
+  formEl.resetFields();
 };
 function handlePasswordFocus() {
-    const birdLogo = document.querySelector(".bird-logo");
+  const birdLogo = document.querySelector(".bird-logo");
 
-    birdLogo!.classList.add("password");
+  birdLogo!.classList.add("password");
 }
 function handlePasswordBlur() {
-    const birdLogo = document.querySelector(".bird-logo");
+  const birdLogo = document.querySelector(".bird-logo");
 
-    birdLogo!.classList.remove("password");
+  birdLogo!.classList.remove("password");
 }
 function sendCode() {
-    isSendCode.value = true;
-    //发送验证码
-    sendTimer.value = CODE_TIMEOUT;
 
-    timer.value = setInterval(() => {
-        sendTimer.value = (sendTimer.value as number) - 1;
-        if (sendTimer.value === 0) {
-            sendTimer.value = "获取验证码";
-            isSendCode.value = false;
-            timer.value && clearTimeout(timer.value);
-        }
-    }, 1000);
+  sendVerifyCode({ email: ruleForm.value.email })
+  isSendCode.value = true;
+  //发送验证码
+  sendTimer.value = CODE_TIMEOUT;
+
+  timer.value = setInterval(() => {
+    sendTimer.value = (sendTimer.value as number) - 1;
+    if (sendTimer.value === 0) {
+      sendTimer.value = "获取验证码";
+      isSendCode.value = false;
+      timer.value && clearTimeout(timer.value);
+    }
+  }, 1000);
 }
 onUnmounted(() => {
-    timer.value && clearTimeout(timer.value);
+  timer.value && clearTimeout(timer.value);
 });
 </script>
 <style scoped>
 :deep(.el-input__wrapper) {
-    background-color: #f0f1f4;
-    color: #606266;
+  background-color: #f0f1f4;
+  color: #606266;
 }
+
 .bird-logo {
-    height: 76px;
+  height: 76px;
 }
 
 .brid-body {
-    position: absolute;
+  position: absolute;
 
-    left: 50%;
-    background-image: url(@/assets/img/bird-body.svg);
-    width: 90px;
-    height: 95px;
-    margin-left: -45px;
+  left: 50%;
+  background-image: url(@/assets/img/bird-body.svg);
+  width: 90px;
+  height: 95px;
+  margin-left: -45px;
 
-    background-repeat: no-repeat;
-    background-position: 50%;
-    background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50%;
+  background-size: cover;
 }
+
 .brid-claw {
-    position: absolute;
-    height: 24px;
-    width: 24px;
-    background-color: #3292dc;
-    transform: scaleY(0.6);
-    border-radius: 50%;
-    bottom: -12px;
-    z-index: 1;
-    transition: 0.3s ease-in-out;
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  background-color: #3292dc;
+  transform: scaleY(0.6);
+  border-radius: 50%;
+  bottom: -12px;
+  z-index: 1;
+  transition: 0.3s ease-in-out;
 }
+
 .brid-claw.left {
-    left: 20%;
+  left: 20%;
 }
+
 .brid-claw.right {
-    right: 20%;
+  right: 20%;
 }
+
 .brid-claw::before {
-    content: "";
-    width: 100%;
-    height: 100%;
-    display: block;
-    /* opacity: 0; */
-    transition: 0.3s ease-out;
-    background-image: url("data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20width='39.54309999999998'%20height='42.504880000000014'%20viewBox='0%200%2039.54309999999998%2042.504880000000014'%20fill='none'%3e%3cdefs%3e%3clinearGradient%20id='linear_0'%20x1='85.2613983223369%25'%20y1='4.589073066433789%25'%20x2='10.164352314310205%25'%20y2='27.141777602948192%25'%20gradientUnits='objectBoundingBox'%3e%3cstop%20offset='0'%20stop-color='%234091F7'%20stop-opacity='1'%20/%3e%3cstop%20offset='1'%20stop-color='%233BAAFB'%20stop-opacity='1'%20/%3e%3c/linearGradient%3e%3c/defs%3e%3cpath%20id='%E8%B7%AF%E5%BE%84%203'%20fill-rule='evenodd'%20fill='url(%23linear_0)'%20transform='translate(0%200)%20rotate(0%2019.77155%2021.25244)'%20opacity='1'%20d='M22.75,0C2.76,0%200,27.8%200,42.5L28.54,42.5C28.74,35.83%2028.79,29.79%2030.44,24.5C33,16.33%2035.6,10.26%2039.54,6C35.54,4%2030.44,7%2028.54,8.5C28.54,6%2030.04,3%2033.04,1.5C25.54,-0.5%2021.25,4.5%2019.25,7C19.25,4.5%2020.54,2%2022.75,0Z%20'%20/%3e%3cpath%20id='%E8%B7%AF%E5%BE%84%203'%20fill-rule='evenodd'%20style='fill:%23000000'%20transform='translate(0%200)%20rotate(0%2019.77155%2021.25244)'%20opacity='0.1'%20d='M22.75,0C2.76,0%200,27.8%200,42.5L28.54,42.5C28.74,35.83%2028.79,29.79%2030.44,24.5C33,16.33%2035.6,10.26%2039.54,6C35.54,4%2030.44,7%2028.54,8.5C28.54,6%2030.04,3%2033.04,1.5C25.54,-0.5%2021.25,4.5%2019.25,7C19.25,4.5%2020.54,2%2022.75,0Z%20'%20/%3e%3c/svg%3e");
-    background-size: contain;
-    background-repeat: no-repeat;
+  content: "";
+  width: 100%;
+  height: 100%;
+  display: block;
+  /* opacity: 0; */
+  transition: 0.3s ease-out;
+  background-image: url("data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20width='39.54309999999998'%20height='42.504880000000014'%20viewBox='0%200%2039.54309999999998%2042.504880000000014'%20fill='none'%3e%3cdefs%3e%3clinearGradient%20id='linear_0'%20x1='85.2613983223369%25'%20y1='4.589073066433789%25'%20x2='10.164352314310205%25'%20y2='27.141777602948192%25'%20gradientUnits='objectBoundingBox'%3e%3cstop%20offset='0'%20stop-color='%234091F7'%20stop-opacity='1'%20/%3e%3cstop%20offset='1'%20stop-color='%233BAAFB'%20stop-opacity='1'%20/%3e%3c/linearGradient%3e%3c/defs%3e%3cpath%20id='%E8%B7%AF%E5%BE%84%203'%20fill-rule='evenodd'%20fill='url(%23linear_0)'%20transform='translate(0%200)%20rotate(0%2019.77155%2021.25244)'%20opacity='1'%20d='M22.75,0C2.76,0%200,27.8%200,42.5L28.54,42.5C28.74,35.83%2028.79,29.79%2030.44,24.5C33,16.33%2035.6,10.26%2039.54,6C35.54,4%2030.44,7%2028.54,8.5C28.54,6%2030.04,3%2033.04,1.5C25.54,-0.5%2021.25,4.5%2019.25,7C19.25,4.5%2020.54,2%2022.75,0Z%20'%20/%3e%3cpath%20id='%E8%B7%AF%E5%BE%84%203'%20fill-rule='evenodd'%20style='fill:%23000000'%20transform='translate(0%200)%20rotate(0%2019.77155%2021.25244)'%20opacity='0.1'%20d='M22.75,0C2.76,0%200,27.8%200,42.5L28.54,42.5C28.74,35.83%2028.79,29.79%2030.44,24.5C33,16.33%2035.6,10.26%2039.54,6C35.54,4%2030.44,7%2028.54,8.5C28.54,6%2030.04,3%2033.04,1.5C25.54,-0.5%2021.25,4.5%2019.25,7C19.25,4.5%2020.54,2%2022.75,0Z%20'%20/%3e%3c/svg%3e");
+  background-size: contain;
+  background-repeat: no-repeat;
 }
 
 .bird-logo.password .brid-claw.left {
-    left: 50%;
-    margin-left: -33px;
+  left: 50%;
+  margin-left: -33px;
 }
 
 .bird-logo.password .brid-claw {
-    bottom: -2px;
-    height: 32px;
-    width: 28px;
-    transform: scale(1);
-    background-color: transparent;
+  bottom: -2px;
+  height: 32px;
+  width: 28px;
+  transform: scale(1);
+  background-color: transparent;
 }
+
 .bird-logo.password .brid-claw.right {
-    right: 50%;
-    margin-right: -33px;
+  right: 50%;
+  margin-right: -33px;
 }
+
 .bird-logo.password .brid-claw::before {
-    opacity: 1;
+  opacity: 1;
 }
+
 .bird-logo.password .brid-claw.right::before {
-    transform: rotateY(180deg);
+  transform: rotateY(180deg);
 }
 </style>
