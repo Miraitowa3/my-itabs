28f0cca44b7d27aa0a17e34f27edf58198ea35f4
miraitowa <1835110799@qq.com>
2025-03-24 08:13:32 +0800

    init

---
+++ src/views/layout/components/IconContentMenu.vue
@@ -0,0 +1,117 @@
+<template>
+    <ContentMenu :containerRef="target" :hanlderCallback="hanlderCallback" ref="contentMenu">
+        <ul class="menu-list">
+            <li class="menu-item hover" @click.stop="iconEdit">
+                <i class="item-icon d-icon">
+                    <svg-icon name="menu-edit" />
+                </i>
+                <span class="menu-item-title text-[12px]">编辑</span>
+            </li>
+
+            <li class="menu-item hover" @click.stop="emits('delete', index)">
+                <i class="item-icon d-icon">
+                    <svg-icon name="menu-del" />
+                </i>
+                <span class="menu-item-title text-[12px]">移除</span>
+            </li>
+        </ul>
+    </ContentMenu>
+    <AddComponent v-model="showEdit" :info="info" :index="index" />
+</template>
+
+<script lang="ts" setup>
+import { useSiderStore } from "@/stores/global";
+import AddComponent from "./AddComponent.vue";
+import { cloneDeep } from "lodash-es";
+const contentMenu = ref<any>();
+const siderStore = useSiderStore();
+const { navConfig, cur } = storeToRefs(siderStore);
+
+const emits = defineEmits(["delete"]);
+const showEdit = ref(false);
+const info = ref<any>();
+const props = defineProps<{
+    target: string;
+    // list: Array<any>;
+}>();
+
+let index = ref(0);
+function hanlderCallback(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    let sarr = [];
+    if (target.getAttribute("cs")) {
+        sarr = target.getAttribute("cs")?.split("_") as string[];
+    } else {
+        sarr = target.parentElement?.getAttribute("cs")?.split("_") as string[];
+    }
+    index.value = Number(sarr[sarr.length - 1]);
+    return true;
+}
+
+function iconEdit() {
+    info.value = cloneDeep(navConfig.value[index.value]);
+
+    showEdit.value = true;
+}
+function closeMenu() {
+    contentMenu.value.closeMenu();
+}
+defineExpose({ closeMenu });
+</script>
+<style scoped>
+.menu-item-title {
+    margin-left: 4px;
+}
+.menu-item {
+    transition: background 0.2s;
+    font-size: 12px;
+    line-height: 30px;
+    color: #fff;
+    padding: 0 4px;
+    cursor: pointer;
+    margin: 4px 0;
+    border-radius: 6px;
+}
+.search-icon {
+    color: rgb(209, 209, 209);
+    font-size: 12px;
+}
+.item-icon {
+    width: 20px;
+    vertical-align: -3px;
+    font-size: 15px;
+}
+.d-icon {
+    --color: inherit;
+    height: 1em;
+    width: 1em;
+    line-height: 1em;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    fill: indexrentColor;
+    color: var(--color);
+}
+.contextmenu-layout em {
+    font-size: 12px;
+    height: 20px;
+    line-height: 20px;
+    width: 36px;
+    background-color: #ffffff1a;
+    border-radius: 12px;
+    text-align: center;
+    margin: 0 2px 4px;
+    indexsor: pointer;
+    float: left;
+    transition: background-color 0.25s;
+}
+.menu-item.hover:hover {
+    background-color: #ffffff1a;
+}
+.contextmenu-layout em:hover {
+    background-color: #fff3;
+}
+.contextmenu-layout em.active {
+    background-color: #ffffff4d;
+}
+</style>
---
+++ src/views/layout/components/components/EditIcon.vue
@@ -0,0 +1,26 @@
+<template>
+    <Dialog
+        v-model:show="show"
+        width="740px"
+        :overlay="false"
+        :customStyle="{
+            backdropFilter: 'blur(18px)',
+            background: 'rgba(200, 200, 200, 0.5)',
+            padding: '16px',
+        }"
+        :closeIcon="false"
+    >
+        <h2>编辑图标</h2>
+        <main><IconCustom :info="info" :index="cur" @close="show = false" /></main>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import IconCustom from "./IconCustom.vue";
+const props = defineProps<{
+    info: any;
+    cur: number;
+}>();
+const show = defineModel();
+</script>
+<style lang="scss" scoped></style>
