5977c3dd7d1250329863d11e014c2ecfa9bd29a6
miraitowa <fengbb@sucsoft.com>
2025-04-07 18:29:58 +0800

    init

--- src/views/layout/Index.vue
+++ src/views/layout/Index.vue
@@ -3,14 +3,15 @@
         <itab-wallpaper :url="wallpaper.src"> </itab-wallpaper>
     </Teleport>
     <AppSider></AppSider>
-    <AppMain></AppMain>
+    <AppMain></AppMain><Nodes v-model="show" />
 </template>
 
 <script setup lang="ts">
 import AppMain from "./components/AppMain.vue";
 import AppSider from "./components/AppSider.vue";
 import { useBaseConfigStore } from "@/stores/baseConfig";
-
+import Nodes from "./components/Nodes.vue";
+const show = ref(true);
 const baseConfigStore = useBaseConfigStore();
 const { wallpaper } = storeToRefs(baseConfigStore);
 </script>
